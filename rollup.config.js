0f3e8a5322a3c2b636f1c83fa1453bbe95bb49f7
Gabriel Pan Gantes <gabrielpanga@gmail.com>
2020-10-20 11:11:21 -0300

    chore: add rollup config

---
+++ rollup.config.js
@@ -0,0 +1,44 @@
+const resolve = require('rollup-plugin-node-resolve');
+const commonjs = require('rollup-plugin-commonjs');
+const license = require('rollup-plugin-license');
+const json = require('rollup-plugin-json');
+const { terser } = require('rollup-plugin-terser');
+
+const pkg = require('./package.json');
+
+const OUTPUT_PATH = 'static';
+
+const PLUGINS = [
+  resolve(),
+  commonjs(),
+  json(),
+  terser({
+    compress: { warnings: false },
+    output: { comments: false },
+    mangle: false
+  }),
+  license({
+    banner: `
+    <%= pkg.name %> v<%= pkg.version %>
+    Author: Pluggy
+    Date: <%= moment().format('YYYY-MM-DD') %>
+    License: MIT
+    `
+  })
+];
+
+export default [
+  {
+    input: 'dist/index.js',
+    output: [
+      {
+        name: 'pluggy',
+        file: `${OUTPUT_PATH}/pluggy.js`,
+        format: 'umd',
+        sourcemap: true,
+        exports: 'named'
+      },
+    ],
+    plugins: PLUGINS
+  },
+];
