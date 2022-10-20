50f3fb5f9636c934e15305f84a87205f4622f68e
NicolasMontone <montonenicolas01@gmail.com>
2022-10-20 18:51:21 -0300

    fix(version): change pluggy-js version from 0.8.1 to 0.8.3 to match current version.

--- src/version.ts
+++ src/version.ts
@@ -1,4 +1,4 @@
 // Project versions
 // Needs to be updated manually since in client-side, we don't have access to the package.json file.
-export const PLUGGY_JS_VERSION = '0.8.1'
+export const PLUGGY_JS_VERSION = '0.8.3'
 export const AXIOS_VERSION = '^0.21.1'
