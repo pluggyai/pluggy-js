4218a6a7f0a640d1ebe05bbe56c34faf435c7b56
NicolasMontone <montonenicolas01@gmail.com>
2022-11-29 15:06:00 -0300

    fix(version): bump patch version.

--- src/version.ts
+++ src/version.ts
@@ -1,4 +1,4 @@
 // Project versions
 // Needs to be updated manually since in client-side, we don't have access to the package.json file.
-export const PLUGGY_JS_VERSION = '0.9.0'
+export const PLUGGY_JS_VERSION = '0.9.1'
 export const AXIOS_VERSION = '^0.21.1'
