a1ec9f30832727383d8f00f7759bd2d705aee621
Nicolás Montone <83733486+NicolasMontone@users.noreply.github.com>
2023-12-07 15:32:00 -0300

    fix(Connector): add property supportsPaymentInitiation in type Connector. (#48)

--- src/version.ts
+++ src/version.ts
@@ -1,3 +1,3 @@
 // Project versions
 // Needs to be updated manually since in client-side, we don't have access to the package.json file.
-export const PLUGGY_JS_VERSION = '0.18.3'
+export const PLUGGY_JS_VERSION = '0.18.4'
